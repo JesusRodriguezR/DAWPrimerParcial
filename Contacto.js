c1f7b2c03a5e263d3bf3eb38373f652301556796
CharlieV1lla <57996207+CharlieV1lla@users.noreply.github.com>
2021-09-12 20:48:53 -0500

    version 1.0 #3

--- Contacto.js
+++ Contacto.js
@@ -1,53 +1,19 @@
-var fields = {email,question}
-
-document.addEventListener("DOMContentLoaded", function() {
-    fields.email = document.getElementById('email');
-    fields.question = document.getElementById('question');
-   })
-
-   function isNotEmpty(value) {
-    if (value == null || typeof value == 'undefined' ) return false;
-    return (value.length > 0);
-   }
-
-   function isEmail(email) {
-    let regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
-    return regex.test(String(email).toLowerCase());
-   }
-
-   function fieldValidation(field, validationFunction) {
-    if (field == null) return false;
-   
-    let isFieldValid = validationFunction(field.value)
-    if (!isFieldValid) {
-    field.className = 'placeholderRed';
-    } else {
-    field.className = '';
-    }
-   
-    return isFieldValid;
-   }
-
-   function isValid() {
-    var valid = true;
-
-    valid &= fieldValidation(fields.email, isEmail);
-    valid &= fieldValidation(fields.question, isNotEmpty);
-   
-    return valid;
-   }
-
-   class User {
-    constructor(email, question) {
-    this.email = email;
-    this.question = question;
-    }
-   }
-
-function sendContact(){
-    if(isValid()){
-        let usr = new User(email.value, question.value);
-
-        alert()
-    }
-}
+        (function() {
+            // https://dashboard.emailjs.com/admin/integration
+            emailjs.init('user_vrkjLBGIfnm0FkXpHz3iI');
+        })();
+        window.onload = function() {
+            document.getElementById('contact-form').addEventListener('submit', function(event) {
+                event.preventDefault();
+                // generate a five digit number for the contact_number variable
+                this.contact_number.value = Math.random() * 100000 | 0;
+                // these IDs from the previous steps
+                emailjs.sendForm('service_o4hjzj8', 'contact_form', this)
+                    .then(function() {
+                        console.log('SUCCESS!');
+                    }, function(error) {
+                        console.log('FAILED...', error);
+                    });
+            });
+        }
+  
